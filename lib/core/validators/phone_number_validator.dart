66a624b1e5f93e22f0a2bc2f4f18429716e888e3
Zarif Muhtasim <2502.zarif@gmail.com>
2024-12-17 06:42:37 +0600

    fix: update phone number validator to require 10 digits

--- lib/core/validators/phone_number_validator.dart
+++ lib/core/validators/phone_number_validator.dart
@@ -2,7 +2,7 @@ String? phoneNumberValidator(String? phoneNumber) {
   if (phoneNumber == null || phoneNumber.isEmpty) {
     return 'Cannot be empty';
   }
-  if (phoneNumber.length != 8) {
+  if (phoneNumber.length != 10) {
     return 'Invalid phone number';
   }
   return null;
